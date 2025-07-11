5563efb36e8a1883ce53cc56c983d184b176e872
Sumith Bandela <sumithbandela@gmail.com>
2025-07-11 16:48:48 -0400

    commir

--- backend/codeigniter4-framework-v4.6.1-0-gd021b04/codeigniter4-framework-d021b04/app/Models/EmployeeModel.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Models;
-use CodeIgniter\Model;
-
-class EmployeeModel extends Model{
-    protected $table = "employees";
-    protected $primaryKey = "id";
-    protected $allowedFields = ['firstname','lastname','email','phone','photo','salary','designation','joined_date'];
-    protected $useTimestamps = true;
-}
